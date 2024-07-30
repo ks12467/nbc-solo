0419e0c51c35abe8bf6ffca8054858b8334bf6f9
Robbie <robbie331@>
2024-07-30 23:45:46 +0900

    level 1-2

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -6,14 +6,18 @@ public class App {
 
     public static void main(String[] args) {
         /*Scanner를 사용하여 입력 값을 받을 수 있음 */
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         // 첫 번째 숫자 입력을 요청하고, 입력된 값을 num1 변수에 저장합니다.
         System.out.print("첫 번째 숫자를 입력하시오. : ");
-        int num1 = sc.nextInt();
+        int num1 = scanner.nextInt();
         //두 번째 숫자 입력을 요청하고, 입력된 값을 num1 변수에 저장합니다.
         System.out.print("두 번째 숫자를 입력하시오. : ");
-        int num2 = sc.nextInt();
+        int num2 = scanner.nextInt();
+
+        // 사용자에게 사칙연산 기호 입력을 요청하고, 입력된 첫 번째 문자를 operator 변수에 저장합니다.
+        System.out.print("사칙연산 기호를 입력하세요. : ");
+        char operator = scanner.next().charAt(0);
 
     }
 }
